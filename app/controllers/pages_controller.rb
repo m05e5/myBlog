8d0c554032331845a7ee0f7d94baf7ad5bbb8be2
m05e5 <calebmoses1111@gmail.com>
2021-11-02 15:56:55 +0100

    create app

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,4 @@
+class PagesController < ApplicationController
+  def post
+  end
+end
---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get post" do
+    get pages_post_url
+    assert_response :success
+  end
+end
