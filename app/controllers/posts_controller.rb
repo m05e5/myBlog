8427e3bb0239f1bcad89381ab2e7cc54d0303422
m05e5 <calebmoses1111@gmail.com>
2021-11-10 11:10:05 +0100

    did the post/index view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,11 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.where(user_id: params[:user_id]).order(created_at: :desc)
+    @user = User.find_by(id: params[:user_id])
+  end
+
+  def comments_per_post(post_id)
+    Comment.where(post_id: post_id).limit(5).order(created_at: :desc)
   end
 
   def show; end
